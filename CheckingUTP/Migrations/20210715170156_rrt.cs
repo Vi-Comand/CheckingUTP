bf579f8e1a70b54ed421d886f3db4ba7b7c3e8ec
Влад Кравченко <kravchenko.vi@live.com>
2021-07-15 20:55:20 +0300

    починил перетягивания, внешний вид

---
+++ CheckingUTP/Migrations/20210715170156_rrt.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CheckingUTP.Migrations
+{
+    public partial class rrt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
