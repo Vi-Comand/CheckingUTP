84240da69898373285200abda4eb2c89d8070708
kravchenko_v_e <kravcenko.vi@live.com>
2021-07-15 19:24:34 +0300

    дизайн начало

--- CheckingUTP/Migrations/20210715121738_4114.Designer.cs
+++ CheckingUTP/Migrations/20210715121738_4114.Designer.cs
@@ -2,14 +2,16 @@
 using CheckingUTP.Models.DataBase;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CheckingUTP.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20210715121738_4114")]
+    partial class _4114
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
